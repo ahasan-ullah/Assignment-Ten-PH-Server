01d50914a7f383cec4ba0c271086510753840885
ahasan-ullah <ahasanullah451@gmail.com>
2024-12-06 19:06:27 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const port=process.env.PORT || 5000;
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+  res.send("basic server is running");
+});
+
+app.listen(port,()=>{
+  console.log("basic server setup done");
+})
